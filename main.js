555a0826978c9561dc144b218f4dc8466bfa672b
pwarrengrovesjr <pwjrg28@gmail.com>
2024-08-15 22:22:42 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,39 @@
+//Whale Talk - Dev30 Project
+
+var input = 'Education is the key.';
+input = input.toLowerCase();
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+var resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    const inputLetters = input[i];
+    // console.log(inputLetters)
+
+    if (inputLetters === 'e') {
+        resultArray.push(input[i]);
+    } 
+
+    if (inputLetters === 'u') {
+        resultArray.push(input[i])
+    }
+
+    for (let j = 0; j < vowels.length; j++) {
+        const vowelLetters = vowels[j];
+        // console.log(vowelLetters)
+
+        if (inputLetters === vowelLetters) {
+            // console.log(inputLetters)
+            
+            resultArray.push(vowelLetters);
+        }
+    }
+};
+
+// console.log(resultArray);
+
+var resultString = resultArray.join('');
+resultString = resultString.toUpperCase()
+
+console.log(resultString)
